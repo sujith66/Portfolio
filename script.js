8d1236947d3034fa643b74dad30223e9816af99a
Sujith <sujiths066@gmail.com>
2020-09-27 13:04:57 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,28 @@
+let theme = localStorage.getItem("theme");
+if (theme === null) {
+  setTheme("light");
+} else {
+  setTheme(theme);
+}
+
+let themeDots = document.getElementsByClassName("theme-dot");
+
+for (let i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener("click", function () {
+    let mode = this.dataset.mode;
+    setTheme(mode);
+  });
+}
+
+function setTheme(mode) {
+  if (mode === "light") {
+    document.getElementById("theme-style").href = "style.css";
+  } else if (mode === "blue") {
+    document.getElementById("theme-style").href = "blue.css";
+  } else if (mode === "green") {
+    document.getElementById("theme-style").href = "green.css";
+  } else if (mode === "purple") {
+    document.getElementById("theme-style").href = "purple.css";
+  }
+  localStorage.setItem("theme", mode);
+}
